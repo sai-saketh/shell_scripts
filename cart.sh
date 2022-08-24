914e3276a6888732171791eefeeae4632de78f63
Sai Saketh Chimmili     /NEW /ISEC/NAVI MUMBAI <739328@icicisecurities.com>
2022-08-24 15:04:39 +0530

    new files added

---
+++ cart.sh
@@ -0,0 +1,7 @@
+printf "WELCOME TO CART\n"
+
+printf "ENTER YOUR LIST ITEMS : "
+read -a items
+printf "TOP 2 items are ${items[0]} and ${items[1]}"
+
+
---
+++ user.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+printf "WELCOME, To register, Provide your username and password\n"
+
+read -p "USERNAME : " usr
+read -sp "PASSWORD : " psd
+
+printf "\n"
+
+echo "REGISTRATION SUCCESSFULL"
